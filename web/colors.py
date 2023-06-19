7c01eb5258114ac10641e2f62d6efa4f44e4cc9e
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2023-06-19 20:41:19 +0200

    Updated colors.py

--- web/colors.py
+++ web/colors.py
@@ -3,7 +3,7 @@ from base import FxControls
 from components import typography as fxType
 from components import block as fxCode
 
-pallet_container = ft.Container()
+pallet_list: list = []
 
 # black & white pallete
 bw_pallet = [
@@ -78,6 +78,30 @@ for color_name in color_names:
         temp_list.append(f"{color_name}{color_number}")
     color_list.append(temp_list)
 
+for i, list_of_color in enumerate(color_list):
+    row = ft.ResponsiveRow(
+        run_spacing=10,
+        alignment="center",
+        vertical_alignment="center",
+        controls=[fxType.subtitle(f"{color_names[i].capitalize()}")],
+    )
+    row_container = ft.Container(content=row, alignment=ft.alignment.center)
+
+    for each_color in list_of_color:
+        color_container = ft.Container(
+            border_radius=10,
+            aspect_ratio=1.25,
+            col={"xs": 3.175, "sm": 2.25, "md": 2.25, "lg": 1, "xl": 1},
+            bgcolor=each_color,
+            tooltip=f"{each_color}",
+            data=each_color,
+            on_click=lambda e: e.page.set_clipboard(e.control.data),
+        )
+        row.controls.append(color_container)
+
+    pallet_list.append(row_container)
+    pallet_list.append(ft.Divider(height=5, color="transparent"))
+
 
 class FxView(ft.View):
     def __init__(
@@ -121,9 +145,11 @@ class FxView(ft.View):
             ft.Divider(height=35, color="transparent"),
             ft.Divider(height=25, color="transparent"),
             # start your layout design here ...
-            fxType.subtitle(f"Black & White", key=1),
+            fxType.subtitle(f"Black & White"),
             ft.Divider(height=5, color="transparent"),
             ft.Container(content=bw_row, alignment=ft.alignment.center),
+            ft.Divider(height=5, color="transparent"),
+            ft.Column(controls=pallet_list),
             # end your layout design here ...
             ft.Divider(height=15, color="transparent"),
         ]
